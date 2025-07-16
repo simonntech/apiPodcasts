5ccf560ce8b9a81bf2aed41936f3abf69fb223f6
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-16 13:28:15 -0300

    refatorando camadas

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,7 +1,11 @@
+import { IncomingMessage } from "http";
 import { repoPodcast } from "../repositories/podcasts-repository"
 
-export const serviceFilterEpisodes = async (podcastName: string) => {
-    const data = await repoPodcast(podcastName);
+export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+    
+    const queryString = podcastName?.split("?p=")[1] || "";
+
+    const data = await repoPodcast(queryString);
 
     return data;
 }
