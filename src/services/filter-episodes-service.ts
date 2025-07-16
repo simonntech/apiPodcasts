9e4563d3e8acdc4002bd4533218badc59c36dc79
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-16 17:06:46 -0300

    erro status code

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,11 +1,27 @@
 import { IncomingMessage } from "http";
 import { repoPodcast } from "../repositories/podcasts-repository"
+import { FilterPodcastModel } from "../models/filter-podcast-model";
+import { statusCode } from "../utils/status-code";
 
-export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
-    
+export const serviceFilterEpisodes = async (
+    podcastName: string | undefined
+): Promise <FilterPodcastModel> => {
+
+    //interface de retorno
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: [],
+    };
+
+    //buscando os dados
     const queryString = podcastName?.split("?p=")[1] || "";
-
     const data = await repoPodcast(queryString);
 
-    return data;
+    //verificação de conteúdo
+
+    responseFormat.statusCode = data.length !== 0 ? statusCode.OK : statusCode.NO_CONTENT;
+
+    responseFormat.body = data;
+
+    return responseFormat;
 }
