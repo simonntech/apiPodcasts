83e721dd1f128f46bd471f577c204c23f60c30ac
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-16 11:23:30 -0300

    iniciando server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import * as http from "http";
+
+//criando servidor
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
+});
+
+//porta conexão
+const port = process.env.PORT
+server.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`)
+})
